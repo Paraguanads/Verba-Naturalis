9b06bdaf20e5cbb66a458eacf3c94e69d893b133
Eliécer Hernández Falcón <paraguanads@gmail.com>
2023-01-09 18:27:28 -0400

    Improvements
    Sentiment Analysis was added but is not working atm.
    Some improvements as well on the static index.
    Tokenization works neat, but an index and cloud with words will be added as well.

--- main.py
+++ main.py
@@ -16,6 +16,25 @@ model = BartForConditionalGeneration.from_pretrained('bart-large-cnn')
 model.to(device)
 tokenizer = BartTokenizer.from_pretrained('bart-large-cnn')
 
+# Function to predict sentiment on text
+def predict_sentiment(text, model, tokenizer, device):
+    input_ids = tokenizer.encode(text, return_tensors='pt').to(device)
+    # Add a batch dimension to the input tensor
+    input_ids = input_ids.unsqueeze(0)
+    logits = model(input_ids)[0]
+    # Remove the batch dimension from the logits tensor
+    logits = logits.squeeze(0)
+    # Convert logits to a Python scalar
+    logits = logits.tolist()[0]
+    if logits > 0:
+        return "positive"
+    else:
+        return "negative"
+
+# Sentiment analysis
+from nltk.sentiment import SentimentIntensityAnalyzer
+sia = SentimentIntensityAnalyzer()
+
 # Summarize a text
 def generate_summary(text, model, tokenizer, device, max_length=1024):
     input_ids = tokenizer.encode(text, return_tensors='pt').to(device)
@@ -23,7 +42,7 @@ def generate_summary(text, model, tokenizer, device, max_length=1024):
     summary_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary_text
 
-# main function, processing the input with the selected option, also print errors for debug purposes
+# main function, processing the input with the selected option, also print errors for debugging purposes
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -46,8 +65,18 @@ def home():
                 return render_template("index.html", error=str(e))
 
             return render_template("index.html", summary=summary)
+        elif request.form["submit"] == "sentiment":
+            try:
+                sentiment = predict_sentiment(model, tokenizer, device, text)
+            except Exception as e:
+                print(e)  # Print error if exists
+                return render_template("index.html", error=str(e))
+
+            return render_template("index.html", sentiment=sentiment)
 
     return render_template("index.html")
+
 # Run the app
 if __name__ == "__main__":
     app.run()
+
